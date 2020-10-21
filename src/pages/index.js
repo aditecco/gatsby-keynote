2dd62fe718313994fb7e569bb0983e937029410b
Alessandro Di Tecco <alessandro@ditecco.me>
2020-10-21 02:08:25 +0200

    Updated placeholder template

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,16 @@ export default function Cover() {
   return (
     <Slide title="Cover">
       <img src={""} alt="" width="300" height="auto" />
+      <h1
+        style={{
+          fontFamily: "Helvetica, sans-serif",
+          fontSize: 160,
+          fontWeight: 300,
+          color: "whitesmoke",
+        }}
+      >
+        Cover
+      </h1>
     </Slide>
   )
 }
